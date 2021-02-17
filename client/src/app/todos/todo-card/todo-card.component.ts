d88a274ce23ed8cab0c09367d555e3786b49fc72
Biruk <mengi024@morris.umn.edu>
2021-02-17 16:28:51 -0600

    Add detail card html layout and styling #3
    Co-authored-by:@MorrisSchoolJM17

--- client/src/app/todos/todo-card/todo-card.component.ts
+++ client/src/app/todos/todo-card/todo-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todo-card',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoCardComponent implements OnInit {
 
+
   constructor() { }
+
+  @Input() todo : Todo;
 
   ngOnInit(): void {
   }
